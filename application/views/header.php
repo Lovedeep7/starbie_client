4c47c848d4a134060d1ea30ace941b906162c593
simmi1987 <54468284+simmi1987@users.noreply.github.com>
2019-09-20 13:26:47 +1000

    Menu updated

--- application/views/header.php
+++ application/views/header.php
@@ -303,7 +303,7 @@
 	<div class="alert alert-danger alert-dismissible fade show" role="alert">
 		<strong>Failed</strong> <?php echo $this->session->flashdata('failed');?>
 		<button type="button" class="close" data-dismiss="alert" aria-label="Close">
-			<span aria-hidden="true">&times;</span>
+			
 		</button>
 		</div>
 <?php } ?>
