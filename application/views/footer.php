c032acbf08f7556b44c186db2098afb69178e664
manishakeshwer <54478028+manishakeshwer@users.noreply.github.com>
2019-09-24 18:33:01 +1000

    changes in menu

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,145 +1,18 @@
 <footer>
-		<div class="footer-top-first">
-			<div class="container py-md-5 py-sm-4 py-3">
-			
-				<div class="row w3l-grids-footer border-top border-bottom py-sm-4 py-3">
-					
-					<div class="col-md-4 offer-footer my-md-0 my-4" style="margin-left:405px;">
-						<div class="row">
-							<div class="col-4 icon-fot">
-								<i class="fas fa-shipping-fast"></i>
-							</div>
-							<div class="col-8 text-form-footer">
-								<h3>Fast Delivery</h3>
-								<p>World Wide</p>
-							</div>
-						</div>
-					</div>
-				
-				</div>
-				<!-- //footer second section -->
-			</div>
-		</div>
-		<!-- footer third section -->
-		<div class="w3l-middlefooter-sec">
-			<div class="container py-md-5 py-sm-4 py-3">
-				<div class="row footer-info w3-agileits-info">
-					<!-- footer categories -->
-					<div class="col-md-3 col-sm-6 footer-grids">
-						<h3 class="text-white font-weight-bold mb-3">Categories</h3>
-						<ul>
-							<li class="mb-3">
-								<a href="#">category 1 </a>
-							</li>
-							<li class="mb-3">
-								<a href="#">category 2</a>
-							</li>
-							<li class="mb-3">
-								<a href="#">category 3</a>
-							</li>
-							<li class="mb-3">
-								<a href="#">category 4</a>
-							</li>
-							<li class="mb-3">
-								<a href="#">category 5</a>
-							</li>
-							<li>
-								<a href="#">category 6</a>
-							</li>
-						</ul>
-					</div>
-					<!-- //footer categories -->
-					<!-- quick links -->
-					<div class="col-md-3 col-sm-6 footer-grids mt-sm-0 mt-4">
-						<h3 class="text-white font-weight-bold mb-3">Quick Links</h3>
-						<ul>
-							<li class="mb-3">
-								<a href="#">About Us</a>
-							</li>
-							<li class="mb-3">
-								<a href="#">Contact Us</a>
-							</li>
-							<li class="mb-3">
-								<a href="#">Help</a>
-							</li>
-							<li class="mb-3">
-								<a href="#">Faqs</a>
-							</li>
-							<li class="mb-3">
-								<a href="#">Terms of use</a>
-							</li>
-							<li>
-								<a href="#">Privacy Policy</a>
-							</li>
-						</ul>
-					</div>
-					<div class="col-md-3 col-sm-6 footer-grids mt-md-0 mt-4">
-						<h3 class="text-white font-weight-bold mb-3">Get in Touch</h3>
-						<ul>
-							<li class="mb-3">
-								<i class="fas fa-map-marker"></i> Brisbane, Australia.</li>
-							<li class="mb-3">
-								<i class="fas fa-mobile"></i> 333 222 3333 </li>
-							<li class="mb-3">
-								<i class="fas fa-phone"></i> +222 11 4444 </li>
-							<li class="mb-3">
-								<i class="fas fa-envelope-open"></i>
-								<a href="mailto:example@mail.com"> mail info1@starbie.com</a>
-							</li>
-							<li>
-								<i class="fas fa-envelope-open"></i>
-								<a href="mailto:example@mail.com"> mail info2@starbie.com</a>
-							</li>
-						</ul>
-					</div>
-					<div class="col-md-3 col-sm-6 footer-grids w3l-agileits mt-md-0 mt-4">
-					
-						<!-- social icons -->
-						<div class="footer-grids  w3l-socialmk mt-3">
-							<h3 class="text-white font-weight-bold mb-3">Follow Us on</h3>
-							<div class="social">
-								<ul>
-									<li>
-										<a class="icon fb" href="#">
-											<i class="fab fa-facebook-f"></i>
-										</a>
-									</li>
-									<li>
-										<a class="icon tw" href="#">
-											<i class="fab fa-twitter"></i>
-										</a>
-									</li>
-									<li>
-										<a class="icon gp" href="#">
-											<i class="fab fa-google-plus-g"></i>
-										</a>
-									</li>
-								</ul>
-							</div>
-						</div>
-						<!-- //social icons -->
-					</div>
-				</div>
-				<!-- //quick links -->
-			</div>
-		</div>
+		
 
-				
-		</div>
-
-	</footer>
-
-	<footer>
 	<div class="copy-right py-3">
 		<div class="container">
 			<p class="text-center text-white">Starbie
 				<a href="#"> An Ecommerce Website</a>
 			</p>
-			
+			<a class="text-center text-white" href="#">Terms & Conditions</a>
 		</div>
 	</div>
 
 	<script src="<?php echo base_url();?>ci_assets/js/jquery-2.2.3.min.js"></script>
+
+
 	<script src="<?php echo base_url();?>ci_assets/js/bootstrap.js"></script>
 	
 </body>
