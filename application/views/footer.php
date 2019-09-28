a5fd76b72c3ae52b0726f429375df90ec9b1413c
manishakeshwer <54478028+manishakeshwer@users.noreply.github.com>
2019-09-28 21:23:55 +1000

    Update footer.php

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,18 +1,436 @@
 <footer>
+		<div class="footer-top-first">
+		<div class="row w3l-grids-footer border-top border-bottom py-sm-4 py-3">
+					<div class="col-md-4 offer-footer">
+						<div class="row">
+							<div class="col-4 icon-fot">
+								<i class="fas fa-dolly"></i>
+							</div>
+							<div class="col-8 text-form-footer">
+								<h3>Free Shipping</h3>
+								
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 offer-footer my-md-0 my-4">
+						<div class="row">
+							<div class="col-4 icon-fot">
+								<i class="fas fa-shipping-fast"></i>
+							</div>
+							<div class="col-8 text-form-footer">
+								<h3>Fast Delivery</h3>
+							
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 offer-footer">
+						<div class="row">
+							<div class="col-4 icon-fot">
+								<i class="far fa-thumbs-up"></i>
+							</div>
+							<div class="col-8 text-form-footer">
+								<h3>Huge Offers</h3>
+								
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- //footer second section -->
+			</div>
+		</div>
+		<div class="agile-sometext py-md-5 py-sm-4 py-3">
+			<div class="container">
+				<!-- brands -->
+				<div class="sub-some">
+				<center><h5 class="font-weight-bold mb-2">Products</h5></center>
+
+				<h5 class="font-weight-bold mb-2">Clothing :</h5>
+					<ul>
+						<li class="m-sm-1">
+							<a href="#" class="border-right pr-2">T-shirts & polos</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="#" class="border-right pr-2">Formals & Casual shirts</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="#" class="border-right pr-2">Kurtis</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="#" class="border-right pr-2">Tops & Tees</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="#" class="border-right pr-2">Jeans</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="#" class="border-right pr-2">Jackets & Hoodies</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="#" class="border-right pr-2">Casual Trousers</a>
+						</li>
+					
+					</ul>
+				
+					<h5 class="font-weight-bold mb-2">Mobile & Tablets :</h5>
+					<ul>
+						<li class="m-sm-1">
+							<a href="#" class="border-right pr-2">Android Phones</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="#" class="border-right pr-2">Smartphones</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="#" class="border-right pr-2">Feature Phones</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="#" class="border-right pr-2">Unboxed Phones</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="#" class="border-right pr-2">Refurbished Phones</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="#" class="border-right pr-2"> Tablets</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="#" class="border-right pr-2">CDMA Phones</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="#" class="border-right pr-2">Value Added Services</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="#" class="border-right pr-2">Sell Old</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="#" class="border-right pr-2">Used Mobiles</a>
+						</li>
+					</ul>
+				</div>
+				<div class="sub-some mt-4">
+					<h5 class="font-weight-bold mb-2">Computers :</h5>
+					<ul>
+						<li class="m-sm-1">
+							<a href="#" class="border-right pr-2">Laptops </a>
+						</li>
+						<li class="m-sm-1">
+							<a href="#" class="border-right pr-2">Printers</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="#" class="border-right pr-2">Routers</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="#" class="border-right pr-2">Ink & Toner Cartridges</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="#" class="border-right pr-2">Monitors</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="#" class="border-right pr-2">Video Games</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="#" class="border-right pr-2">Unboxed & Refurbished Laptops</a>
+						</li>
+						<li>
+							<a href="#" class="border-right pr-2">Assembled Desktops</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="product2.html" class="border-right pr-2">Data Cards</a>
+						</li>
+					</ul>
+				</div>
+				<div class="sub-some mt-4">
+					<h5 class="font-weight-bold mb-2">TV, Audio & Large Appliances :</h5>
+					<ul>
+						<li class="m-sm-1">
+							<a href="product2.html" class="border-right pr-2">TVs & DTH</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="product2.html" class="border-right pr-2">Home Theatre Systems</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="product2.html" class="border-right pr-2">Hidden Cameras & CCTVs</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="product2.html" class="border-right pr-2">Refrigerators</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="product2.html" class="border-right pr-2">Washing Machines</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="product2.html" class="border-right pr-2"> Air Conditioners</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="product2.html" class="border-right pr-2">Cameras</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="product2.html" class="border-right pr-2">Speakers</a>
+						</li>
+					</ul>
+				</div>
+				<div class="sub-some mt-4">
+					<h5 class="font-weight-bold mb-2">Mobile & Laptop Accessories :</h5>
+					<ul>
+						<li class="m-sm-1">
+							<a href="product2.html" class="border-right pr-2">Headphones</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="#" class="border-right pr-2">Power Banks </a>
+						</li>
+						<li class="m-sm-1">
+							<a href="#" class="border-right pr-2">Backpacks</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="#" class="border-right pr-2">Mobile Cases & Covers</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="#" class="border-right pr-2">Pen Drives</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="product2.html" class="border-right pr-2">External Hard Disks</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="product2.html" class="border-right pr-2"> Mouse</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="product2.html" class="border-right pr-2">Smart Watches & Fitness Bands</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="product2.html" class="border-right pr-2">MicroSD Cards</a>
+						</li>
+					</ul>
+				</div>
+				<div class="sub-some mt-4">
+					<h5 class="font-weight-bold mb-2">Appliances :</h5>
+					<ul>
+						<li class="m-sm-1">
+							<a href="product2.html" class="border-right pr-2">Trimmers</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="product2.html" class="border-right pr-2">Hair Dryers</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="product2.html" class="border-right pr-2">Emergency Lights</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="product2.html" class="border-right pr-2">Water Purifiers </a>
+						</li>
+						<li class="m-sm-1">
+							<a href="product2.html" class="border-right pr-2">Electric Kettles</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="product2.html" class="border-right pr-2">Hair Straighteners</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="product2.html" class="border-right pr-2">Induction Cooktops</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="product2.html" class="border-right pr-2">Sewing Machines</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="product2.html" class="border-right pr-2"> Geysers</a>
+						</li>
+					</ul>
+				</div>
+				<div class="sub-some mt-4">
+					<h5 class="font-weight-bold mb-2">Popular on Electro Store</h5>
+					<ul>
+						<li class="m-sm-1">
+							<a href="#" class="border-right pr-2">Offers & Coupons</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="product2.html" class="border-right pr-2">Couple Watches</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="product2.html" class="border-right pr-2">Gas Stoves</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="product2.html" class="border-right pr-2"> Air Coolers</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="product2.html" class="border-right pr-2">Air Purifiers</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="product2.html" class="border-right pr-2">Headphones</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="product2.html" class="border-right pr-2"> Headsets</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="product2.html" class="border-right pr-2">Pressure Cookers</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="product2.html" class="border-right pr-2">Sandwich Makers</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="product2.html" class="border-right pr-2">Air Friers</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="product2.html" class="border-right pr-2">Irons</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="product2.html" class="border-right pr-2">LED TV</a>
+						</li>
+						<li class="m-sm-1">
+							<a href="product2.html" class="border-right pr-2">Sandwich Makers</a>
+						</li>
+					</ul>
+				</div>
+				<!-- //brands -->
+				<!-- payment -->
+				<div class="sub-some child-momu mt-4">
+					<h5 class="font-weight-bold mb-3">Payment Method</h5>
+					<ul>
+						<li>
+							<img src="<?php echo base_url();?>ci_assets/images/pay2.png" alt="">
+						</li>
+						<li>
+							<img src="<?php echo base_url();?>ci_assets/images/pay5.png" alt="">
+						</li>
+						<li>
+							<img src="<?php echo base_url();?>ci_assets/images/pay1.png" alt="">
+						</li>
+						<li>
+							<img src="<?php echo base_url();?>ci_assets/images/pay4.png" alt="">
+						</li>
+						<li>
+							<img src="<?php echo base_url();?>ci_assets/images/pay6.png" alt="">
+						</li>
+						<li>
+							<img src="<?php echo base_url();?>ci_assets/images/pay3.png" alt="">
+						</li>
+						<li>
+							<img src="<?php echo base_url();?>ci_assets/images/pay7.png" alt="">
+						</li>
+						<li>
+							<img src="<?php echo base_url();?>ci_assets/images/pay8.png" alt="">
+						</li>
+						<li>
+							<img src="<?php echo base_url();?>ci_assets/images/pay9.png" alt="">
+						</li>
+					</ul>
+				</div>
+				<!-- //payment -->
+			</div>
+		</div>
+		<!-- footer third section -->
+		<div class="w3l-middlefooter-sec">
+			<div class="container py-md-5 py-sm-4 py-3">
+				<div class="row footer-info w3-agileits-info">
+					<!-- footer categories -->
+					<div class="col-md-3 col-sm-6 footer-grids">
+						<h3 class="text-white font-weight-bold mb-3">Categories</h3>
+						<ul>
+							<li class="mb-3">
+								<a href="#">category 1 </a>
+							</li>
+							<li class="mb-3">
+								<a href="#">category 2</a>
+							</li>
+							<li class="mb-3">
+								<a href="#">category 3</a>
+							</li>
+							<li class="mb-3">
+								<a href="#">category 4</a>
+							</li>
+							<li class="mb-3">
+								<a href="#">category 5</a>
+							</li>
+							<li>
+								<a href="#">category 6</a>
+							</li>
+						</ul>
+					</div>
+					<!-- //footer categories -->
+					<!-- quick links -->
+					<div class="col-md-3 col-sm-6 footer-grids mt-sm-0 mt-4">
+						<h3 class="text-white font-weight-bold mb-3">Quick Links</h3>
+						<ul>
+							<li class="mb-3">
+								<a href="#">About Us</a>
+							</li>
+							<li class="mb-3">
+								<a href="#">Contact Us</a>
+							</li>
+							<li class="mb-3">
+								<a href="#">Help</a>
+							</li>
+							<li class="mb-3">
+								<a href="#">Faqs</a>
+							</li>
+							<li class="mb-3">
+								<a href="#">Terms of use</a>
+							</li>
+							<li>
+								<a href="#">Privacy Policy</a>
+							</li>
+						</ul>
+					</div>
+					<div class="col-md-3 col-sm-6 footer-grids mt-md-0 mt-4">
+						<h3 class="text-white font-weight-bold mb-3">Get in Touch</h3>
+						<ul>
+							<li class="mb-3">
+								<i class="fas fa-map-marker"></i> Brisbane, Australia.</li>
+							<li class="mb-3">
+								<i class="fas fa-mobile"></i> 333 222 3333 </li>
+							<li class="mb-3">
+								<i class="fas fa-phone"></i> +222 11 4444 </li>
+							<li class="mb-3">
+								<i class="fas fa-envelope-open"></i>
+								<a href="mailto:example@mail.com"> mail info1@starbie.com</a>
+							</li>
+							<li>
+								<i class="fas fa-envelope-open"></i>
+								<a href="mailto:example@mail.com"> mail info2@starbie.com</a>
+							</li>
+						</ul>
+					</div>
+					<div class="col-md-3 col-sm-6 footer-grids w3l-agileits mt-md-0 mt-4">
+					
+						<!-- social icons -->
+						<div class="footer-grids  w3l-socialmk mt-3">
+							<h3 class="text-white font-weight-bold mb-3">Follow Us on</h3>
+							<div class="social">
+								<ul>
+									<li>
+										<a class="icon fb" href="#">
+											<i class="fab fa-facebook-f"></i>
+										</a>
+									</li>
+									<li>
+										<a class="icon tw" href="#">
+											<i class="fab fa-twitter"></i>
+										</a>
+									</li>
+									<li>
+										<a class="icon gp" href="#">
+											<i class="fab fa-google-plus-g"></i>
+										</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+						<!-- //social icons -->
+					</div>
+				</div>
+				<!-- //quick links -->
+			</div>
+		</div>
+
+				
+		</div>
 		
 
+	</footer>
+
+	<footer>
 	<div class="copy-right py-3">
 		<div class="container">
 			<p class="text-center text-white">Starbie
 				<a href="#"> An Ecommerce Website</a>
 			</p>
-			<a class="text-center text-white" href="#">Terms & Conditions</a>
+			
 		</div>
 	</div>
 
 	<script src="<?php echo base_url();?>ci_assets/js/jquery-2.2.3.min.js"></script>
-
-
 	<script src="<?php echo base_url();?>ci_assets/js/bootstrap.js"></script>
 	
 </body>
