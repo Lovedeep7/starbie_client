5125e395f13d2f7d95e8b62b60d8a3b040fad0c4
preet2026 <55168692+preet2026@users.noreply.github.com>
2019-12-14 23:43:41 +1000

    changes in aboutus.php

--- application/views/aboutus.php
+++ application/views/aboutus.php
@@ -1,4 +1,21 @@
-<!-- about -->
+<div class="page-head_agile_info_w3l">
+	</div>
+	<!-- //banner-2 -->
+	<!-- page -->
+	<div class="services-breadcrumb">
+		<div class="agile_inner_breadcrumb">
+			<div class="container">
+				<ul class="w3_short">
+					<li>
+						<a href="<?php echo base_url();?>">Home</a>
+						<i>|</i>
+					</li>
+					<li>About Us</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<!-- about -->
 	<div class="welcome py-sm-5 py-4">
 		<div class="container py-xl-4 py-lg-2">
 			<!-- tittle heading -->
@@ -10,12 +27,11 @@
 				<div class="col-lg-6 welcome-left">
 					<h3>Welcome</h3>
 					<h4 class="my-sm-3 my-2">Starbie is an ecommerce webkit which allows your business to sell products and services to their online audience, as well as make business decision. Having an ecommerce platform helps you to increase conversion rate since people get a chance to immediately buy from you rather than wait to visit the store. </h4>
-					<p>ecommerce websites are online portals that facilitate online transactions of goods and services through means of the transfer of information and funds over the Internet.  Now, with a single website, anything and everything that a transaction needs can be executed online.</p>
+					<p>ecommerce websites are online portals that facilitate online transactions of goods and services through means of the transfer of information and funds over the Internet.  Now, with a single website, anything and everything that a transaction needs can be executed online.</p>		
 				</div>
 				<div class="col-lg-6 welcome-right-top mt-lg-0 mt-sm-5 mt-4">
-					<img src="<?php echo base_url();?>ci_assets/images/ec1.png" class="img-fluid" alt=" ">
+					<img src="<?php echo base_url();?>ci_assets/images/ec1.png" class="img-fluid" alt=" " style="max-width: 130%;">
 				</div>
 			</div>
 		</div>
 	</div>
-	
