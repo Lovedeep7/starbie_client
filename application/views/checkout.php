bd6d8d32214e77b7ce4198cebdcfd3eacb03d9f6
manishakeshwer <54478028+manishakeshwer@users.noreply.github.com>
2019-12-18 13:43:59 +1000

    changes in checkout.php

--- application/views/checkout.php
+++ application/views/checkout.php
@@ -20,9 +20,7 @@
 	<div class="privacy py-sm-5 py-4">
 		<div class="container py-xl-4 py-lg-2">
 			<!-- tittle heading -->
-			<h3 class="tittle-w3l text-center mb-lg-5 mb-sm-4 mb-3">
-				<span>C</span>heckout
-			</h3>
+			
 			<!-- //tittle heading -->
 			<div class="checkout-right">
 				<h4 class="mb-sm-4 mb-3">Your shopping cart contains:
