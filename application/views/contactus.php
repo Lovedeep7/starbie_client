647b43a5e589352517ce998c439a2541e714e9a3
Lovedeep7 <46017105+Lovedeep7@users.noreply.github.com>
2020-01-10 23:41:51 +1000

    Update contactus.php

--- application/views/contactus.php
+++ application/views/contactus.php
@@ -1,3 +1,21 @@
+<div class="page-head_agile_info_w3l">
+
+	</div>
+	<!-- //banner-2 -->
+	<!-- page -->
+	<div class="services-breadcrumb">
+		<div class="agile_inner_breadcrumb">
+			<div class="container">
+				<ul class="w3_short">
+					<li>
+						<a href="<?php echo base_url();?>">Home</a>
+						<i>|</i>
+					</li>
+					<li>Contact Us</li>
+				</ul>
+			</div>
+		</div>
+	</div>
 <?php if($this->session->flashdata('success')) { ?>
 			<div class="alert alert-success alert-dismissible fade show" role="alert">
 		<strong><?php echo $this->session->userdata('user_firstname'); ?></strong> <?php echo $this->session->flashdata('success');?>
@@ -21,11 +39,72 @@
 				<span>U</span>s
 			</h3>
 			<!-- //tittle heading -->
-			
-			<h4 class="font-weight-bold mt-sm-4 mt-3 mb-3">Address</h4>
+			<div class="row contact-grids agile-1 mb-5">
+				<div class="col-sm-4 contact-grid agileinfo-6 mt-sm-0 mt-2">
+					<div class="contact-grid1 text-center">
+						<div class="con-ic">
+							<i class="fas fa-map-marker-alt rounded-circle"></i>
+						</div>
+						<h4 class="font-weight-bold mt-sm-4 mt-3 mb-3">Address</h4>
 						<p>Brisbane,
 							<label>Australia.</label>
 						</p>
 					</div>
 				</div>
-				
+				<div class="col-sm-4 contact-grid agileinfo-6 my-sm-0 my-4">
+					<div class="contact-grid1 text-center">
+						<div class="con-ic">
+							<i class="fas fa-phone rounded-circle"></i>
+						</div>
+						<h4 class="font-weight-bold mt-sm-4 mt-3 mb-3">Call Us</h4>
+						<p>+(0121) 121 121
+							<label>+(0121) 121 122</label>
+						</p>
+					</div>
+				</div>
+				<div class="col-sm-4 contact-grid agileinfo-6">
+					<div class="contact-grid1 text-center">
+						<div class="con-ic">
+							<i class="fas fa-envelope-open rounded-circle"></i>
+						</div>
+						<h4 class="font-weight-bold mt-sm-4 mt-3 mb-3">Email</h4>
+						<p>
+							<a href="mailto:info@starbie.com">info1@starbie.com</a>
+							<label>
+								<a href="mailto:info@starbie.com">info2@starbie.com/a>
+							</label>
+						</p>
+					</div>
+				</div>
+			</div>
+			<!-- form -->
+			<form action="<?php echo base_url();?>Ecom/addContact" method="post">
+				<div class="contact-grids1 w3agile-6">
+					<div class="row">
+						<div class="col-md-6 col-sm-6 contact-form1 form-group">
+							<label class="col-form-label">Name</label>
+							<input type="text" class="form-control" name="name" placeholder="" required="">
+						</div>
+						<div class="col-md-6 col-sm-6 contact-form1 form-group">
+							<label class="col-form-label">E-mail</label>
+							<input type="email" class="form-control" name="email" placeholder="" required="">
+						</div>
+					</div>
+					<div class="contact-me animated wow slideInUp form-group">
+						<label class="col-form-label">Message</label>
+						<textarea name="message" class="form-control" placeholder="" required=""> </textarea>
+					</div>
+					<div class="contact-form">
+						<input type="submit" value="Submit" style="background:#F45C5D;">
+					</div>
+				</div>
+			</form>
+			<!-- //form -->
+		</div>
+	</div>
+	<!-- //contact -->
+	<!-- map -->
+	<div class="map mt-sm-0 mt-4">
+		<iframe src="https://maps.google.com/maps?q=Brisbane%2C%20Australia&t=&z=13&ie=UTF8&iwloc=&output=embed" allowfullscreen></iframe>
+	</div>
+	<!--/map-->
