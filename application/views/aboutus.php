b9eb86c3e2dbc16e67df374d5aca14e199dd159c
simmi1987 <54468284+simmi1987@users.noreply.github.com>
2020-01-10 23:34:47 +1000

    Update aboutus.php

--- application/views/aboutus.php
+++ application/views/aboutus.php
@@ -15,5 +15,24 @@
 			</div>
 		</div>
 	</div>
-	
+	<!-- about -->
+	<div class="welcome py-sm-5 py-4">
+		<div class="container py-xl-4 py-lg-2">
+			<!-- tittle heading -->
+			<h3 class="tittle-w3l text-center mb-lg-5 mb-sm-4 mb-3">
+				<span>A</span>bout
+				<span>U</span>s</h3>
+			<!-- //tittle heading -->
+			<div class="row">
+				<div class="col-lg-6 welcome-left">
+					<h3>Welcome</h3>
+					<h4 class="my-sm-3 my-2">Starbie is an ecommerce webkit which allows your business to sell products and services to their online audience, as well as make business decision. Having an ecommerce platform helps you to increase conversion rate since people get a chance to immediately buy from you rather than wait to visit the store. </h4>
+					<p>ecommerce websites are online portals that facilitate online transactions of goods and services through means of the transfer of information and funds over the Internet.  Now, with a single website, anything and everything that a transaction needs can be executed online.</p>		
+				</div>
+				<div class="col-lg-6 welcome-right-top mt-lg-0 mt-sm-5 mt-4">
+					<img src="<?php echo base_url();?>ci_assets/images/ec1.png" class="img-fluid" alt=" " style="max-width: 130%;">
+				</div>
+			</div>
+		</div>
+	</div>
 				
