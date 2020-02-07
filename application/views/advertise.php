b972450e228f465ca0fdfa5396342794ad6dffc5
preet2026 <55168692+preet2026@users.noreply.github.com>
2020-02-07 23:56:16 +1000

    Update advertise.php

--- application/views/advertise.php
+++ application/views/advertise.php
@@ -40,22 +40,8 @@
 				</div>
 			</div>
 			
-			<div class="carousel-item item4">
-				<div class="container">
-					<div class="w3l-space-banner">
-						<div class="carousel-caption p-lg-5 p-sm-4 p-3">
-							<p>Advertise
-								<span>3</span>
-							<h3 class="font-weight-bold pt-2 pb-lg-5 pb-4">
-								<span>Big</span>
-								Sale
-							</h3>
-							<a class="button2" href="<?php echo base_url();?>ci_assets/product.html">Shop Now </a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+			
+		
 		<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
 			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
 			<span class="sr-only">Previous</span>
